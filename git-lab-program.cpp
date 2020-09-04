033246d6a0aa43a2314838af946b7be560227981
Connor Petty <cp944317@ohio.edu>
2020-09-04 15:28:53 -0400

    pushing program

---
+++ git-lab-program.cpp
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Connor Petty
+ *        Date: 9/4/2020
+ * Description: hello git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
